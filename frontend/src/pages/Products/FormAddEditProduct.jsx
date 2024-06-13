5914e2b85ed98a1420da4ff9443467a55042f926
AldairBryan <asalcedoch@unsa.edu.pe>
2024-06-13 10:41:24 -0500

    Minor fixes to add edit product

--- frontend/src/pages/Products/FormAddEditProduct.jsx
+++ frontend/src/pages/Products/FormAddEditProduct.jsx
@@ -4,7 +4,7 @@ import { getProduct, createProduct, updateProduct } from '../../api/Product.api'
 import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
-function FormAddEditProduct({loadProducts}) {
+function FormAddEditProduct() {
   const navigate = useNavigate();
   const { id } = useParams();
   const isEditing = Boolean(id);
@@ -28,13 +28,11 @@ function FormAddEditProduct({loadProducts}) {
     try {
         if(isEditing){
           await updateProduct(product.id, data);
-          loadProducts;
-          toast.success('Successfully created product');
           navigate('/products');
+          toast.success('Successfully created product');
         } else {
           data.orderProducts = [];
           await createProduct(data);
-          loadProducts;
           toast.success('Error while editing the product');
           navigate('/products');
         }
@@ -60,41 +58,62 @@ function FormAddEditProduct({loadProducts}) {
     loadOrder();
   }, [id]);
 
+  const styles = {
+    flexContainer: {
+      display: 'flex',
+      justifyContent: 'space-between',
+    },
+    flexItem: {
+      flex: '1',
+      margin: '0 5px',
+    },
+  };
+  
   return (
-    <>
-    <h2>{isEditing ? 'Edit Product' : 'Add Product'}</h2>
-    <form onSubmit={onSubmit}>
-      <label>
-        Name Of The Product
-        <input
-          className='input-text'
-          type="text"
-          placeholder='Name of the Product'
-          {...register('name', { required: true,
-            validate: (value) => validateMaxSize(value),
-          })}
-        />
-        {errors.name?.type === 'required' && <p className='text-error'>*The name field is required</p>}
-        {errors.name?.type === 'validate' && <p className='text-error'>*The name field must not exceed 5 characters</p>}    
-      </label>
-      <label>
-        Unit Price
-        <input
-          className='input-text'
-          type="number"
-          step="0.01"
-          min="0"
-          placeholder='Unit Price'
-          {...register('unitPrice', { required: true})}
-        />
-        {errors.unitPrice?.type === 'required' && <p className='text-error'>*The name Unit Price is required</p>}
-      </label>
-      <div className='contenedor-btn'>
-        <button className='btn-cancelar' type='button' onClick={() => handleCancel()}>Cancel</button>
-        <button className='btn-registrar' type='submit'>Finish</button>
+    <div className='contenedor-componente'>
+      <h2>{isEditing ? 'Edit Product' : 'Add Product'}</h2>
+        <div className='contenedor-crear'>
+          <form onSubmit={onSubmit}>
+            <div style={styles.flexContainer}>
+              <div style={styles.flexItem}>
+                <label>
+                  Name Of The Product
+                  <input
+                    className='input-text'
+                    type="text"
+                    placeholder='Name of the Product'
+                    {...register('name', { required: true,
+                      validate: (value) => validateMaxSize(value),
+                    })}
+                  />
+                  {errors.name?.type === 'required' && <p className='text-error'>*The name field is required</p>}
+                  {errors.name?.type === 'validate' && <p className='text-error'>*The name field must not exceed 5 characters</p>}    
+                </label>
+              </div>
+            </div>
+            <div style={styles.flexContainer}>
+              <div style={styles.flexItem}>
+                <label>
+                  Unit Price
+                  <input
+                    className='input-text'
+                    type="number"
+                    step="0.01"
+                    min="0"
+                    placeholder='Unit Price'
+                    {...register('unitPrice', { required: true})}
+                  />
+                  {errors.unitPrice?.type === 'required' && <p className='text-error'>*The name Unit Price is required</p>}
+                </label>
+              </div>
+            </div>
+            <div className='contenedor-btn'>
+              <button className='btn-cancelar' type='button' onClick={() => handleCancel()}>Cancel</button>
+              <button className='btn-registrar' type='submit'>Finish</button>
+            </div>
+        </form>
       </div>
-    </form>
-    </>
+    </div>
   )
 }
 
